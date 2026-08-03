08b87bac7f01f8e55c4b40e00643e0c3e6229051
Yujun <pathfinder357@github.com>
2026-08-03 09:46:56 +0900

    chore: jpa Auditing + security 설정 / refactor: 현재 글로벌 설정 팀 컨벤션에 맞게 작성(현재 모놀리식구조)

--- src/main/java/com/msa7/v1/V1Application.java
+++ src/main/java/com/msa7/v1/V1Application.java
@@ -2,7 +2,9 @@ package com.msa7.v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class V1Application {
 
