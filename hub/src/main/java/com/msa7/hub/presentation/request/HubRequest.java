4b35a6351ccb7c1f67b0b1c9caf25a936a3c39bd
wootae <comijoa2@naver.com>
2026-08-03 21:46:15 +0900

    feat: HubRequest 유효성 검증 보완

--- hub/src/main/java/com/msa7/hub/presentation/request/HubRequest.java
+++ hub/src/main/java/com/msa7/hub/presentation/request/HubRequest.java
@@ -1,23 +1,32 @@
 package com.msa7.hub.presentation.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public record HubRequest(
 
-    @NotBlank
+    @NotBlank(message = "허브 이름은 필수 값입니다.")
+    @Size(max = 50)
     String name,
 
-    @NotBlank
+    @NotBlank(message = "주소는 필수 값입니다.")
+    @Size(max = 100)
     String address,
 
-    @NotNull
+    @NotNull(message = "위도는 필수 값입니다.")
+    @DecimalMin(value = "-90", message = "위도는 -90 이상이어야 합니다.")
+    @DecimalMax(value = "90", message = "위도는 90 이하여야 합니다.")
     BigDecimal latitude,
 
-    @NotNull
+    @NotNull(message = "경도는 필수 값입니다.")
+    @DecimalMin(value = "-180", message = "경도는 -180 이상이어야 합니다.")
+    @DecimalMax(value = "180", message = "경도는 180 이하여야 합니다.")
     BigDecimal longitude,
 
     UUID centralHubId
