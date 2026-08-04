8ee0738af16a2aa703377ec8e588f7f233a3c2c9
Yujun <pathfinder357@github.com>
2026-08-04 22:09:57 +0900

    refactor: enum 수정

--- order/src/main/java/com/msa7/v1/order/domain/vo/OrderStatus.java
+++ order/src/main/java/com/msa7/v1/order/domain/vo/OrderStatus.java
@@ -1,9 +1,5 @@
 package com.msa7.v1.order.domain.vo;
 
 public enum OrderStatus {
-	PENDING,
-	DELIVERING,
-	CREATED,
-	COMPLETED,
-	CANCELED;
+	PENDING, PROCESSING, SHIPPED, DELIVERED, CANCELED, DELETED
 }
