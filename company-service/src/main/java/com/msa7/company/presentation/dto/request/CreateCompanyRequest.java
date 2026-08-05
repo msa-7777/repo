bf85b6e6cbf4a4782835b21a8f88655e68e6259c
choipg <choipg2684@naver.com>
2026-08-05 10:51:30 +0900

    refactor : queryDSL로 변경

--- company-service/src/main/java/com/msa7/company/presentation/dto/request/CreateCompanyRequest.java
+++ company-service/src/main/java/com/msa7/company/presentation/dto/request/CreateCompanyRequest.java
@@ -3,24 +3,22 @@ package com.msa7.company.presentation.dto.request;
 import com.msa7.company.domain.model.CompanyType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
-@Getter
-@NoArgsConstructor
-public class CreateCompanyRequest {
+import jakarta.validation.constraints.Size;
 
-    @NotBlank(message = "업체명은 필수 입력 항목입니다.")
-    private String name;
+public record CreateCompanyRequest(
+        @NotBlank(message = "업체명은 필수 입력 항목입니다.")
+        @Size(max = 100, message = "업체명은 최대 100자까지 입력 가능합니다.")
+        String name,
 
-    @NotNull(message = "업체 타입은 필수 항목입니다.")
-    private CompanyType type;
+        @NotNull(message = "업체 타입은 필수 입력 항목입니다.")
+        CompanyType type,
 
-    @NotNull(message = "관리 허브 ID는 필수 항목입니다.")
-    private UUID hubId;
+        @NotNull(message = "허브 ID는 필수 입력 항목입니다.")
+        UUID hubId,
 
-    @NotBlank(message = "업체 주소는 필수 입력 항목입니다.")
-    private String address;
+        @NotBlank(message = "주소는 필수 입력 항목입니다.")
+        String address
+) {
 }
