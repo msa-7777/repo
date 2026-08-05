798741b50da2802ebf21a2abd86ae1c4c5714e8b
choipg <choipg2684@naver.com>
2026-08-05 12:29:52 +0900

    refactor : eureka 관련 주석처리

--- company-service/src/main/java/com/msa7/company/infrastructure/client/HubClient.java
+++ company-service/src/main/java/com/msa7/company/infrastructure/client/HubClient.java
@@ -1,23 +1,23 @@
-package com.msa7.company.infrastructure.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.UUID;
-
-@FeignClient(name = "hub-service", url = "${hub-service.url}")
-public interface HubClient {
-    // #TODO : hub 확인 후 수정 요망
-    @GetMapping("/api/hubs/{hubId}")
-    HubResponse getHub(@PathVariable("hubId") UUID hubId);
-
-    default boolean existsHub(UUID hubId) {
-        try {
-            return getHub(hubId) != null;
-        } catch (Exception e) {
-            // 외부 Hub 서비스 연결 실패 시 가동성 확보를 위해 기본값 true 처리 혹은 에러 핸들링
-            return true;
-        }
-    }
-}
+//package com.msa7.company.infrastructure.client;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//import java.util.UUID;
+//
+//@FeignClient(name = "hub-service", url = "${hub-service.url}")
+//public interface HubClient {
+//    // #TODO : hub 확인 후 수정 요망
+//    @GetMapping("/api/hubs/{hubId}")
+//    HubResponse getHub(@PathVariable("hubId") UUID hubId);
+//
+//    default boolean existsHub(UUID hubId) {
+//        try {
+//            return getHub(hubId) != null;
+//        } catch (Exception e) {
+//            // 외부 Hub 서비스 연결 실패 시 가동성 확보를 위해 기본값 true 처리 혹은 에러 핸들링
+//            return true;
+//        }
+//    }
+//}
