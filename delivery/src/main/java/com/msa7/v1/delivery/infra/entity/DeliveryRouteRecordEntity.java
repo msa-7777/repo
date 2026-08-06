3f8c5f3cec471cfe251fbce7a479ae409ade7b43
Yujun <pathfinder357@github.com>
2026-08-06 09:45:52 +0900

    feat: 인프라 도메인 엔티티 jap 어노테이션 누락

--- delivery/src/main/java/com/msa7/v1/delivery/infra/entity/DeliveryRouteRecordEntity.java
+++ delivery/src/main/java/com/msa7/v1/delivery/infra/entity/DeliveryRouteRecordEntity.java
@@ -6,13 +6,22 @@ import com.msa7.v1.delivery.domain.vo.RouteStatus;
 import com.msa7.v1.delivery.infra.persist.BaseEntity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "p_delivery_route_record")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DeliveryRouteRecordEntity extends BaseEntity {
 
 	@Id
