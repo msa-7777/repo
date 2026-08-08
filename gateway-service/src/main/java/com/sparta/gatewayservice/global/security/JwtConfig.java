88180c7b89739baa557d525868b65699770b6514
Ye-jee <yejee0408@gmail.com>
2026-08-08 19:00:50 +0900

    feat: gateway 1차 인증

---
+++ gateway-service/src/main/java/com/sparta/gatewayservice/global/security/JwtConfig.java
@@ -0,0 +1,26 @@
+package com.sparta.gatewayservice.global.security;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder(@Value("${jwt.secret}") String secret) {
+        SecretKey secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+
+        return NimbusReactiveJwtDecoder.withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}
