8c4bda36756ac035b00ca95cf71720be5a6884e1
wootae <comijoa2@naver.com>
2026-08-09 20:14:00 +0900

    chore: 허브 참조 관련 에러 메시지 수정

--- hub/src/main/java/com/msa7/hub/global/exception/ErrorCode.java
+++ hub/src/main/java/com/msa7/hub/global/exception/ErrorCode.java
@@ -28,9 +28,9 @@ public enum ErrorCode {
     CONFLICT(HttpStatus.CONFLICT, "리소스 충돌이 발생했습니다."),
     HUB_ROUTE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 등록된 허브 경로입니다."),
     HUB_REFERENCED_BY_COMPANY(HttpStatus.CONFLICT, "해당 허브를 참조하는 업체가 존재하여 삭제할 수 없습니다."),
-    HUB_REFERENCED_BY_INVENTORY(HttpStatus.CONFLICT, "해당 허브에 재고가 존재하여 삭제할 수 없습니다."),
+    HUB_REFERENCED_BY_INVENTORY(HttpStatus.CONFLICT, "해당 허브를 참조하는 재고가 존재하여 삭제할 수 없습니다."),
     HUB_REFERENCED_BY_CHILD_HUB(HttpStatus.CONFLICT, "해당 허브를 중앙 허브로 참조하는 소속 허브가 존재하여 삭제할 수 없습니다."),
-    HUB_REFERENCED_BY_HUB_ROUTE(HttpStatus.CONFLICT, "해당 허브를 사용하는 허브 경로가 존재하여 삭제할 수 없습니다."),
+    HUB_REFERENCED_BY_HUB_ROUTE(HttpStatus.CONFLICT, "해당 허브를 참조하는 허브 경로가 존재하여 삭제할 수 없습니다."),
     HUB_REFERENCED_BY_USER(HttpStatus.CONFLICT, "해당 허브를 참조하는 유저가 존재하여 삭제할 수 없습니다."),
 
     // 500
