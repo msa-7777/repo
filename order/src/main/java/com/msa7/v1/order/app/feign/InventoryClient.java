9e5cd79da53c3518dc3f07581a2d0de4b8906c38
Yujun <pathfinder357@github.com>
2026-08-10 21:29:31 +0900

    feat: 다른 서비스와의 동기식 연결

--- order/src/main/java/com/msa7/v1/order/app/feign/InventoryClient.java
+++ order/src/main/java/com/msa7/v1/order/app/feign/InventoryClient.java
@@ -1,10 +1,11 @@
-package com.msa7.v1.order.infra.feign;
+package com.msa7.v1.order.app.feign;
 
 import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,4 +14,6 @@ public interface InventoryClient {
 	@GetMapping("/api/v1/inventories/{productId}")
 	void verifyInventory(@PathVariable("productId") UUID productId, @RequestParam("quantity") Integer quantity);
 
+	// @PostMapping("/api/v1/products/{productId}/verify-stock")
+	// void verifyProductAndStock(@PathVariable("productId") UUID productId, @RequestParam("quantity") Integer quantity);
 }
