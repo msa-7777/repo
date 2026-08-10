5ddf5088018e057808e2cb8a9c9ccbe289b4ad65
choipg <choipg2684@naver.com>
2026-08-10 10:50:30 +0900

    fix : UUID 수정

--- ai-service/src/main/java/com/msa7/ai/infrastructure/client/product/ProductResponse.java
+++ ai-service/src/main/java/com/msa7/ai/infrastructure/client/product/ProductResponse.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ProductResponse(
-        Long productId,
+        UUID productId,
         String name,
         UUID companyId
 ) {}
