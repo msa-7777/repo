4681cfed6a46a3bcde32d233ac1943d0e1a2d82d
Yujun <pathfinder357@github.com>
2026-08-10 21:32:38 +0900

    feat: 애플리케이션 api 로직 구현

--- order/src/main/java/com/msa7/v1/order/app/OrderService.java
+++ order/src/main/java/com/msa7/v1/order/app/OrderService.java
@@ -1,42 +1,45 @@
 package com.msa7.v1.order.app;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.msa7.v1.order.app.feign.DeliveryClient;
 import com.msa7.v1.order.domain.aggregate.Order;
 import com.msa7.v1.order.domain.repo.OrderRepo;
-import com.msa7.v1.order.infra.feign.InventoryClient;
+import com.msa7.v1.order.app.feign.InventoryClient;
 import com.msa7.v1.order.infra.publisher.OrderEventPub;
+import com.msa7.v1.order.presentation.dto.onlycontoller.RestApiResponse;
+import com.msa7.v1.order.presentation.dto.payload.DeliveryResponse;
 import com.msa7.v1.order.presentation.dto.payload.OrderCreatedEvent;
+import com.msa7.v1.order.presentation.dto.payload.OrderWithDeliveryDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
 	private final OrderRepo orderRepo;
 	private final InventoryClient inventoryClient;
-	private final OrderEventPub orderEventPub;
+	private final DeliveryClient deliveryClient;
 
 	@Transactional
-	public Order createOrder(UUID receiverId, UUID productId, Integer quantity, String requests) {
+	public Order createOrder(UUID receiverId, UUID productId, Integer quantity, String requests,
+		UUID receiverSlackId, UUID startHubId, UUID endHubId, String destinationAddress
+	) {
 		// 재고 확인 inventory에서 요청(필요에 따라 동기 유지 또는 이벤트 전환)
 		inventoryClient.verifyInventory(productId, quantity);
 
-		// 도메인 객체 생성 및 저장
-		Order order = Order.create(receiverId, productId, quantity, requests);
-		Order savedOrder = orderRepo.save(order);
-
-		// 배송 생성 요청
-		OrderCreatedEvent event = new OrderCreatedEvent(savedOrder.getId(), receiverId, requests);
-		orderEventPub.publishOrderCreated(event);
-
-		return savedOrder;
+		// 도메인 생성 (이벤트 등록)
+		Order order = Order.create(receiverId, productId, quantity, requests
+		, receiverSlackId, startHubId, endHubId, destinationAddress);
+		return orderRepo.save(order);
 	}
-
 
 	@Transactional
 	public Order getOrder(UUID orderId) {
@@ -44,33 +47,40 @@ public class OrderService {
 	}
 
 	@Transactional
-	public Order updateOrder(UUID orderId, Integer quantity, String requests) {
-		Order order = getOrder(orderId);
-		order.update(quantity, requests);
-		return orderRepo.save(order);
-	}
-
-	@Transactional
-	public void cancelOrder(UUID orderId) {
-		Order order = getOrder(orderId);
-		order.cancel();
+	public void compeleteOrderSaga(UUID orderId, UUID deliveryId) {
+		Order order = orderRepo.findById(orderId).orElseThrow(() -> new IllegalArgumentException("Order not found"));
+		order.startDelivery(deliveryId);
 		orderRepo.save(order);
 	}
 
 	@Transactional
-	public void deleteOrder(UUID orderId) {
-		Order order = getOrder(orderId);
-		order.delete();
-		orderRepo.save(order); // 상태 업데이트해서 softDelete
-	}
-
-	public void startDelivery(UUID uuid) {
-	}
-
-	@Transactional
-	public void compensateOrder(UUID orderId) {
-		Order order = getOrder(orderId);
-		order.cancel();
+	public void failOrderSaga(UUID orderId, String reason) {
+		Order order = orderRepo.findById(orderId).orElseThrow(() -> new IllegalArgumentException("Order not found"));
+		order.cancelOrder();
 		orderRepo.save(order);
+		// Todo: 시간이 남으면 인펜토리 서비스에 재고 복구 트랜잭션 이벤트 해보기
 	}
+
+	// 주문ID만 주면 배송상태를 알수 있는 api(필규님 요청)
+	@Transactional(readOnly = true)
+	public OrderWithDeliveryDto getOrderWithDeliveryStatus(UUID orderId) {
+		Order order = orderRepo.findById(orderId).orElseThrow(() -> new IllegalArgumentException("Order not found"));
+		if (order.getDeliveryId() == null) {
+			return new OrderWithDeliveryDto(order, "배송 정보 없음");
+		}
+		String deliveryStatus;
+		try {
+			RestApiResponse<DeliveryResponse> response = deliveryClient.
+				getDeliveryInfo(order.getDeliveryId());
+				deliveryStatus = Optional.ofNullable(response)
+					.map(RestApiResponse::data)
+					.map(DeliveryResponse::status)
+					.orElse("배송 정보 없음");
+		} catch (Exception e){
+			log.error("배송 조회 실패 deliveryId: {}", order.getDeliveryId(), e);
+			deliveryStatus = "배송 정보 조회 실패";
+		}
+		return new OrderWithDeliveryDto(order, deliveryStatus);
+	}
+
 }
