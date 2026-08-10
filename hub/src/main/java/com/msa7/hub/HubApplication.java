da20e29a625da9789043cd3e6f636a84c0be4dfd
wootae <comijoa2@naver.com>
2026-08-11 00:57:13 +0900

    feat: hub 권한 체크 로직 구현

--- hub/src/main/java/com/msa7/hub/HubApplication.java
+++ hub/src/main/java/com/msa7/hub/HubApplication.java
@@ -3,9 +3,11 @@ package com.msa7.hub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableMethodSecurity
 public class HubApplication {
 
 	public static void main(String[] args) {
