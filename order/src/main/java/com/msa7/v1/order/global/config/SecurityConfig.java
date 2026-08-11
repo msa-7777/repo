7ec08c4c6a3cd8b2646cc709e82a9924b9c5f173
Yujun <pathfinder357@github.com>
2026-08-11 14:05:03 +0900

    feat: 에러 수정

--- order/src/main/java/com/msa7/v1/order/global/config/SecurityConfig.java
+++ order/src/main/java/com/msa7/v1/order/global/config/SecurityConfig.java
@@ -8,7 +8,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.msa7.v1.delivery.global.security.HeaderAuthenticationFilter;
+
+import com.msa7.v1.order.global.security.HeaderAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
